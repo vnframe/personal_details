be82b3c1ab411fd41cf42951a594b86406cdf823
vnframe <vnframe95@icloud.com>
2017-07-26 14:53:00 -0400

    last name working

--- app.rb
+++ app.rb
@@ -6,40 +6,46 @@ get '/' do
 end
 post '/name' do
   name = params[:user_name]
-  redirect '/age?user_name=' + name
+  last = params[:last_name]
+  redirect '/age?user_name=' + name + '&last_name=' + last
 end
 
 get '/age' do
     name = params[:user_name]
-    erb :get_age, :locals => {:name => name}
+    last = params[:last_name]
+    erb :get_age, :locals => {:name => name, :last => last}
 end
 post '/age' do
   age = params[:user_age]
   name = params[:user_name]
-  redirect '/three_numbers?user_name=' + name + '&age=' + age
+  last = params[:last_name]
+  redirect '/three_numbers?user_name=' + name + '&last' + last + '&age=' + age
 end
 get '/three_numbers' do
   age = params[:age]
   name = params[:user_name]
-  erb :three_numbers, :locals => {:name => name, :age => age}
+  last = params[:last_name]
+  erb :three_numbers, :locals => {:name => name, :age => age, :last => last}
 end
 post '/three_numbers' do
   age = params[:user_age]
   name = params[:user_name]
+  last = params[:last_name]
   num1 = params[:favenum1]
   num2 = params[:favenum2]
   num3 = params[:favenum3]
   totalnumbers = sum(num1.to_i, num2.to_i, num3.to_i)
-  redirect '/total?user_name=' + name + '&user_age=' + age + '&favenum1=' + num1 + '&favenum2=' + num2 + '&favenum3=' + num3
+  redirect 'total?user_name=' + name + '&last_name=' + last + '&user_age=' + age + '&favenum1=' + num1 + '&favenum2=' + num2 + '&favenum3=' + num3
 end
 get "/total" do
   age = params[:user_age]
   name = params[:user_name]
+  last = params[:last_name]
   num1 = params[:favenum1]
   num2 = params[:favenum2]
   num3 = params[:favenum3]
   totalnumbers = params[totalnumbers]
-  erb :total, :locals=>{:name=> name, :age=>age, :favenum1=>num1, :favenum2=>num2, :favenum3=>num3, :totalnumbers=>totalnumbers}
+  erb :total, :locals=>{:name=> name, :last => last, :age=>age, :favenum1=>num1, :favenum2=>num2, :favenum3=>num3, :totalnumbers=>totalnumbers}
 end
 # post 'total' do
 #   age = params[:user_age]
