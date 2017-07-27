dd706a1dc548a3f04366aff44e8e60d46a192487
vnframe <vnframe95@icloud.com>
2017-07-27 13:57:40 -0400

    drink working

--- app.rb
+++ app.rb
@@ -19,34 +19,90 @@ post '/age' do
   age = params[:user_age]
   name = params[:user_name]
   last = params[:last_name]
-  redirect '/three_numbers?user_name=' + name + '&last' + last + '&age=' + age
+  redirect '/haircolor?user_name=' + name + '&last_name=' + last + '&user_age=' + age
 end
-get '/three_numbers' do
-  age = params[:age]
-  name = params[:user_name]
-  last = params[:last_name]
-  erb :three_numbers, :locals => {:name => name, :age => age, :last => last}
-end
-post '/three_numbers' do
+get '/haircolor' do
   age = params[:user_age]
   name = params[:user_name]
   last = params[:last_name]
-  num1 = params[:favenum1]
-  num2 = params[:favenum2]
-  num3 = params[:favenum3]
-  totalnumbers = sum(num1.to_i, num2.to_i, num3.to_i)
-  redirect 'total?user_name=' + name + '&last_name=' + last + '&user_age=' + age + '&favenum1=' + num1 + '&favenum2=' + num2 + '&favenum3=' + num3
+  erb :haircolor, :locals => {:name => name, :last => last, :age => age}
 end
-get "/total" do
+post '/haircolor' do
   age = params[:user_age]
   name = params[:user_name]
   last = params[:last_name]
-  num1 = params[:favenum1]
-  num2 = params[:favenum2]
-  num3 = params[:favenum3]
-  totalnumbers = params[totalnumbers]
-  erb :total, :locals=>{:name=> name, :last => last, :age=>age, :favenum1=>num1, :favenum2=>num2, :favenum3=>num3, :totalnumbers=>totalnumbers}
+  hair = params[:hair_colour]
+  redirect 'eyecolor?user_name='+ name + '&last_name=' + last + '&user_age=' + age + '&hair_colour=' + hair
 end
+get '/eyecolor' do
+  age = params[:user_age]
+  name = params[:user_name]
+  last = params[:last_name]
+  hair = params[:hair_colour]
+  erb :eyecolor, :locals => {:name => name, :last => last, :age => age, :hair => hair}
+ end
+post '/eyecolor' do
+  age = params[:user_age]
+  name = params[:user_name]
+  hair = params[:hair_colour]
+  eyes = params[:eye_colour]
+  last = params[:last_name]
+  redirect  'favfood?user_name=' + name + '&last_name=' + last + '&user_age=' + age + '&hair_colour=' + hair + '&eye_colour=' + eyes
+end
+get '/favfood' do
+  age = params[:user_age]
+  name = params[:user_name]
+  last = params[:last_name]
+  hair = params[:hair_colour]
+  eyes = params[:eye_colour]
+  erb :fav_food, :locals => {:name => name, :last => last, :age => age, :hair => hair, :eyes => eyes}
+end
+post '/favfood' do
+  age = params[:user_age]
+  name = params[:user_name]
+  last = params[:last_name]
+  hair = params[:hair_colour]
+  eyes = params[:eye_colour]
+  food = params[:fav_food]
+  redirect  'favdrink?user_name=' + name + '&last_name=' + last + '&user_age=' + age + '&hair_colour=' + hair + '&eye_colour=' + eyes + '&fav_food=' + food
+end
+
+get '/favdrink' do
+  age = params[:user_age]
+  name = params[:user_name]
+  last = params[:last_name]
+  hair = params[:hair_colour]
+  eyes = params[:eye_colour]
+  food = params[:fav_food]
+  puts "user age is #{age}"
+  erb :fav_drink, :locals => {:name => name, :last => last, :age => age, :hair => hair, :eyes => eyes, :food => food}
+end
+# get '/three_numbers' do
+#   age = params[:age]
+#   name = params[:user_name]
+#   last = params[:last_name]
+#   erb :three_numbers, :locals => {:name => name, :age => age, :last => last}
+# end
+# post '/three_numbers' do
+#   age = params[:user_age]
+#   name = params[:user_name]
+#   last = params[:last_name]
+#   num1 = params[:favenum1]
+#   num2 = params[:favenum2]
+#   num3 = params[:favenum3]
+#   totalnumbers = sum(num1.to_i, num2.to_i, num3.to_i)
+#   redirect 'total?user_name=' + name + '&last_name=' + last + '&user_age=' + age + '&favenum1=' + num1 + '&favenum2=' + num2 + '&favenum3=' + num3
+# end
+# get "/total" do
+#   age = params[:user_age]
+#   name = params[:user_name]
+#   last = params[:last_name]
+#   num1 = params[:favenum1]
+#   num2 = params[:favenum2]
+#   num3 = params[:favenum3]
+#   totalnumbers = params[totalnumbers]
+#   erb :total, :locals=>{:name=> name, :last_name => last, :age=>age, :favenum1=>num1, :favenum2=>num2, :favenum3=>num3, :totalnumbers=>totalnumbers}
+# end
 # post 'total' do
 #   age = params[:user_age]
 #   name = params[:user_name]
